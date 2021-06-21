e1ff134db795563896fdf92a0b5dd1929844c011
Ramana <ramanaptr@Ramanas-MacBook-Pro.local>
2021-06-21 23:03:45 +0800

    Update sample app on init the data using Rx java

--- app/src/main/java/com/ramanaptr/sampleezrecyclerview/MainActivity.kt
+++ app/src/main/java/com/ramanaptr/sampleezrecyclerview/MainActivity.kt
@@ -5,13 +5,20 @@ import android.os.Handler
 import android.os.Looper
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.ramanaptr.widget.EzRecyclerView
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.BackpressureStrategy
+import io.reactivex.rxjava3.core.Flowable
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private val handler = Handler(Looper.getMainLooper())
+    private var subscribe: Disposable? = null
     private lateinit var rvSample: EzRecyclerView<SampleData>
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,8 +26,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         initComponent()
         initListener()
-        initDataWithCustomShimmer()
+        // initDataWithCustomShimmer()
         // initDataWithDefaultShimmer() //TODO: Uncomment for test
+        initDataWithRxJava()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        subscribe?.isDisposed?.apply { if (this) subscribe?.dispose() }
     }
 
     private fun initComponent() {
@@ -29,8 +42,9 @@ class MainActivity : AppCompatActivity() {
 
     private fun initListener() {
         btnRefresh.setOnClickListener {
-            initDataWithCustomShimmer()
+            // initDataWithCustomShimmer()
             // initDataWithDefaultShimmer()
+            initDataWithRxJava()
         }
         stopShimmer.setOnClickListener {
             rvSample.hideShimmer()
@@ -43,23 +57,48 @@ class MainActivity : AppCompatActivity() {
     private fun initDataWithCustomShimmer() {
         // Start Shimmer for waiting for the data
         rvSample.startShimmer(10, R.layout.sample_shimmer_effect, R.id.sample_shimmer)
-        handler.postDelayed(callback, 5000)
+        handler.postDelayed(showData, 5000)
     }
 
     private fun initDataWithDefaultShimmer() {
         // Start Shimmer for waiting for the data
         rvSample.startShimmer(10)
-        handler.postDelayed(callback, 5000)
+        handler.postDelayed(showData, 5000)
     }
 
-    private val callback = {
-        val list = arrayListOf<SampleData>()
+    private fun initDataWithRxJava() {
+        // start shimmer when load the data
+        rvSample.startShimmer(10)
+        // start load the data
+        subscribe = Flowable.create<List<SampleData>>({
+            val dataList = arrayListOf<SampleData>()
+            Handler(Looper.getMainLooper()).postDelayed({
+                for (i in 1..100000) {
+                    dataList.add(SampleData("Key $i", "Value $i"))
+                }
+                it.onNext(dataList)
+                it.onComplete()
+            }, 2000)
+        }, BackpressureStrategy.BUFFER)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                rvSample.setViewHolderLayout(R.layout.sample_view_holder, it, bindViewHolder)
+            }, {
+                it.printStackTrace()
+            }, {
+                Toast.makeText(this, "Completed!", Toast.LENGTH_SHORT).show()
+            })
+    }
+
+    private val showData = {
+        val dataList = arrayListOf<SampleData>()
         for (i in 1..100) {
-            list.add(SampleData("Key $i", "Value $i"))
+            dataList.add(SampleData("Key $i", "Value $i"))
         }
 
         // Re set view Holder
-        rvSample.setViewHolderLayout(R.layout.sample_view_holder, list, bindViewHolder)
+        rvSample.setViewHolderLayout(R.layout.sample_view_holder, dataList, bindViewHolder)
     }
 
     private val bindViewHolder = { view: View, data: SampleData ->
